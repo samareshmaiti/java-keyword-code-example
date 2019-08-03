34c4cc40212a8c215b4b4e0b89f267da191c4cd9
samaresh.maiti <samaresh.maiti@cgi.com>
2019-08-03 12:53:14 +0530

    implementing Lambda expression

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 1.implementation of OOPs Concepts - Object, Class, Interface, Inheritance, Package
         Package Used:com.stackroute.javakeywords.oopsconcept
+        
+ 2.More on Classes and Objects - Anatomy of a class, 
+ Constructors, Creating and using objects, this keyword,
+  Controlling access to a member, Nested Classes, When to use nested,
+  inner, local, anonymus classes and Lambda expressions.
--- src/main/java/com/stackroute/javakeywords/Main.java
+++ src/main/java/com/stackroute/javakeywords/Main.java
@@ -1,16 +1,52 @@
 package com.stackroute.javakeywords;
 
-import com.stackroute.javakeywords.oopsconcept.Tata;
+import com.stackroute.javakeywords.oopsadvance.ConstructorAndThisKeyword;
+import com.stackroute.javakeywords.oopsadvance.MyInterface;
+import com.stackroute.javakeywords.oopsadvance.NestedClassExample;
+
 import com.stackroute.javakeywords.oopsconcept.TataZest;
 
 public class Main {
     public static void main(String[] args) {
         //Creating object of chile class
         TataZest tataZest = new TataZest();
-        //calling parent class methods via child class
+        //calling constructorAndThisKeyword class methods via child class
         tataZest.start();
         tataZest.stop();
         //calling child class method
         tataZest.carDetails();
+        //implementation of ConstructorAndThisKeyword.java class
+        ConstructorAndThisKeyword constructorAndThisKeyword =new ConstructorAndThisKeyword();
+        //set the values to constructorAndThisKeyword class using setter method
+        constructorAndThisKeyword.setName("samaresh");
+        constructorAndThisKeyword.setAge(50);
+        constructorAndThisKeyword.setId(10);
+        //accessing the values using the getter method
+        System.out.println(constructorAndThisKeyword.getName()+"\n"+ constructorAndThisKeyword.getAge()+"\n"+ constructorAndThisKeyword.getId());
+        // accessing a static nested class
+        NestedClassExample.InnerClass1 nestedObject = new NestedClassExample.InnerClass1();
+        nestedObject.display();
+        //creating object of NestedClassExample
+        NestedClassExample nestedClassExample =new NestedClassExample();
+        NestedClassExample.InnerClass2 myInner = nestedClassExample.new InnerClass2();
+        //implementing nested inner class
+        myInner.print();
+        NestedClassExample.InnerClass2 innerClass2=nestedClassExample.new InnerClass2();
+        innerClass2.print();
+        //implementing of lambda expression
+
+//        new Thread(()->{
+//            System.out.println("Lambda");
+//            System.out.println("Another Lambda");
+//            System.out.println("Lambda2");
+//        }
+//        ).start();
+        MyInterface fobj = ()->System.out.println("this is lambda expression");
+        // This calls above lambda expression and prints 10.
+        fobj.print();
+
+
+
+
     }
 }
