6c2d235ca473f6443d07c780ce9466d9f458995d
samaresh.maiti <samaresh.maiti@cgi.com>
2019-08-04 08:55:59 +0530

    implementing byte stream

--- src/main/java/com/stackroute/javakeywords/inputoutputstream/FileInputStreamDemo.java
+++ src/main/java/com/stackroute/javakeywords/inputoutputstream/FileInputStreamDemo.java
@@ -1,6 +1,7 @@
 package com.stackroute.javakeywords.inputoutputstream;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 //this is the implementation of the FileInputStream class that takes input as file location
@@ -9,9 +10,15 @@ public class FileInputStreamDemo {
     public static void main(String args[]) {
         try {
             FileInputStream fileInputStream = new FileInputStream("/home/cgi/Music/Test.txt");
+            FileOutputStream fileOutputStream=new FileOutputStream("/home/cgi/Music/Test.txt");
             int i = 0;
             while ((i = fileInputStream.read()) != -1) {
                 System.out.print((char) i);
+            }
+            int j;
+            while((j=fileInputStream.read())!=-1)
+            {
+                fileOutputStream.write((byte)j);
             }
 
             fileInputStream.close();
